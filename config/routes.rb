f96f04b5a3da2d43684568300dfdc89cef099a44
John Mason <john@johnmason.io>
2017-04-08 21:08:00 -0400

    Only create or list events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :credits
   resources :tokens
-  resources :events
+  resources :events, only: [:index, :create]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
