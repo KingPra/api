b63e060d4172000fb165511edf7056051a9d960b
John Mason <john@johnmason.io>
2017-04-08 21:25:11 -0400

    Assign credit to events if they are creditable

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :user
-  belongs_to :credit
+  belongs_to :credit, optional: true
+
+  before_validation :assign_credit
+
+  delegate :points, to: :credit
+
+  private
+
+  def assign_credit
+    Credit.find_by(name: category).tap do |c|
+      self.credit = c
+    end
+  end
 end
