715e7355954da92cbde9f2552fdb067904f046fb
John Mason <john@johnmason.io>
2017-04-12 10:14:34 -0400

    Create some seeds

--- db/migrate/20170407182452_create_unknown_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateUnknownUser < ActiveRecord::Migration[5.0]
-  def up
-    User.create!(first_name: "unknown", last_name: "user", email: "unknown")
-  end
-
-  def down
-    User.find_by(email: "unknown").destroy
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def step(str)
+  puts "==> #{str}"
+end
+
+def info(str)
+  puts "--- #{str}"
+end
+
+CREDITS = YAML.load_file "config/credits.yml"
+
+CREDITS.each do |attrs|
+  name = attrs["name"]
+  Credit.create_with(attrs).find_or_create_by!(name: name)
+  step "created credit: #{name}"
+end
+info "(#{Credit.count} credits created)"
+
+Token.find_or_create_by!(description: "dev testing")
+step "created token for 'dev testing'"
+
+unknown_user = User.find_or_create_by!(email: "unknown") do |u|
+  u.first_name = "unknown"
+  u.last_name = "unknown"
+end
+step "created unknown user"
+
+random_user = User.find_or_create_by!(email: "random@user.com") do |u|
+  u.first_name = "random"
+  u.last_name = "random"
+end
+step "created random user '#{random_user.email}'"
+
+e = Event.create!(category: CREDITS.sample["name"], user_id: unknown_user.id, info: { source: "db:seed" })
+step "created a #{e.category} event for unknown user"
+
+e = Event.create! category: CREDITS.sample["name"], user_id: random_user.id, info: { source: "db:seed" }
+step "created a #{e.category} event for random user"
+
+info "unknown user credibility: #{unknown_user.credibility}"
+info "random user credibility: #{random_user.credibility}"
