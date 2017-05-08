7906711316e4e452ff5f95ff984f859235a1202b
John Mason <john@johnmason.io>
2017-05-08 14:36:46 -0400

    Change event creation

--- db/migrate/20170407132059_create_events.rb
+++ db/migrate/20170407132059_create_events.rb
@@ -3,6 +3,7 @@ class CreateEvents < ActiveRecord::Migration[5.0]
     create_table :events do |t|
       t.string :category
       t.references :user, foreign_key: true
+      t.integer :quantity, default: 1
       t.json :info, default: {}
 
       t.timestamps
