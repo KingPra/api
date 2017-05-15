3300a6010c1fe681935f63b0803648c6ad621b00
John Mason <john@johnmason.io>
2017-05-14 20:45:17 -0400

    Use wildcard for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:4200", ENV["CLIENT_URL"]
+    origins "localhost:4200",
+      /\A\.*\.charlottejuniordevs\.com\z/,
+      /\A\.*\.herokuapp\.com\z/
 
     resource "*",
              headers: :any,
